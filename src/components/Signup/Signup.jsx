02bcb3abce9bfe0358fa10dad995042ceee1d285
umairayub-dev <umairayub049@gmail.com>
2023-06-07 19:29:08 +0500

    add state in form

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,54 +1,68 @@
-import { Form, Button,Card } from 'react-bootstrap';
+import { useState } from 'react';
+import { Form, Button, Card } from 'react-bootstrap';
 
 const Signup = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [name, setName] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        // do action
+    }
     return (
         <Card className='text-start m-5'>
-        <Form>
-            <h2>CREATE AN ACCOUNT</h2>
-            <Form.Group controlId="name">
-                <Form.Label>Your Name</Form.Label>
-                <Form.Control
-                    type="text"
-                />
-            </Form.Group>
+            <Form onSubmit={handleSubmit}>
+                <h2>CREATE AN ACCOUNT</h2>
+                <Form.Group controlId="name">
+                    <Form.Label>Your Name</Form.Label>
+                    <Form.Control
+                        onChange={(e) => setName(e.target.value)}
+                        value={name}
+                        type="text"
+                    />
+                </Form.Group>
 
-            <Form.Group controlId="email">
-                <Form.Label>Your Email</Form.Label>
-                <Form.Control
-                    type="email"
-                />
-            </Form.Group>
+                <Form.Group controlId="email">
+                    <Form.Label>Your Email</Form.Label>
+                    <Form.Control
+                        onChange={(e) => setEmail(e.target.value)}
+                        value={email}
+                        type="email"
+                    />
+                </Form.Group>
 
-            <Form.Group controlId="password">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                    type="password"
-                />
-            </Form.Group>
+                <Form.Group controlId="password">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        value={password}
+                    />
+                </Form.Group>
 
-            <Form.Group controlId="repeatPassword">
-                <Form.Label>Repeat your password</Form.Label>
-                <Form.Control
-                    type="password"
-                />
-            </Form.Group>
+                <Form.Group controlId="repeatPassword">
+                    <Form.Label>Repeat your password</Form.Label>
+                    <Form.Control
+                        type="password"
+                    />
+                </Form.Group>
 
-            <Form.Group controlId="termsAgreed">
-                <Form.Check
-                    type="checkbox"
-                    label="I agree to all statements in Terms of service"
+                <Form.Group controlId="termsAgreed">
+                    <Form.Check
+                        type="checkbox"
+                        label="I agree to all statements in Terms of service"
+                    />
+                </Form.Group>
 
-                />
-            </Form.Group>
+                <Button variant="primary" type="submit">
+                    Sign Up
+                </Button>
 
-            <Button variant="primary" type="submit">
-                Sign Up
-            </Button>
-
-            <p>
-                Have already an account? <a href="#">Login here</a>
-            </p>
-        </Form>
+                <p>
+                    Have already an account? <a href="#">Login here</a>
+                </p>
+            </Form>
         </Card>
     )
 }
